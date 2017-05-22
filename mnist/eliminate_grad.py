ca69b7b1504eb8795e081b24ec97d13b11b572e0
YoshiteruIshihara <t130440@mail.ryukoku.ac.jp>
2017-05-22 16:10:58 +0900

    [add] add eliminate gradient
    apply measurs
    -dropout
    -weight decay

---
+++ mnist/eliminate_grad.py
@@ -0,0 +1,114 @@
+"""classifier model
+    |affine|→|relu|→|affine|→|relu|→|affine|→|relu|→|dropout|→|cross-entropy|
+    apply measures to eliminate gradient
+    - dropout
+    - weight decay
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import sys, os
+sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
+
+import tensorflow as tf
+from common import *
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+def train():
+  # Import data
+  mnist = input_data.read_data_sets(FLAGS.data_dir,
+                                    one_hot=True,
+                                    fake_data=FLAGS.fake_data)
+
+  sess = tf.InteractiveSession()
+  # Create the model
+  with tf.name_scope('input'):
+    x = tf.placeholder(tf.float32, [None, 784], name='x-input')
+    y_ = tf.placeholder(tf.float32, [None, 10], name='y-input')
+
+  with tf.name_scope('input_reshape'):
+    image_shaped_input = tf.reshape(x, [-1, 28, 28, 1])
+    tf.summary.image('input', image_shaped_input, 10)
+
+  hidden1 = nn_layer(x, 784, 200, 'layer1')
+  hidden2 = nn_layer(hidden1, 200, 100, 'layer2')
+  hidden3 = nn_layer(hidden2, 100, 500, 'layer3')
+
+  with tf.name_scope('dropout'):
+    keep_prob = tf.placeholder(tf.float32)
+    tf.summary.scalar('dropout_keep_probability', keep_prob)
+    dropped = tf.nn.dropout(hidden3, keep_prob)
+  # Do not apply softmax activation yet, see below.
+  y = nn_layer(dropped, 500, 10, 'dropout', act=tf.identity)
+
+  with tf.name_scope('cross_entropy'):
+    diff = tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y)
+    with tf.name_scope('total'):
+      cross_entropy = tf.reduce_mean(diff)
+    with tf.name_scope('weight_decay'):
+      tf.add_to_collection('losses', cross_entropy)
+      loss = tf.add_n(tf.get_collection('losses'), name='total_loss')
+  tf.summary.scalar('loss', loss)
+
+  with tf.name_scope('train'):
+    train_step = tf.train.AdamOptimizer(FLAGS.learning_rate).minimize(
+        loss)
+
+  with tf.name_scope('accuracy'):
+    with tf.name_scope('correct_prediction'):
+      correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+    with tf.name_scope('accuracy'):
+      accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+  tf.summary.scalar('accuracy', accuracy)
+
+  merged = tf.summary.merge_all()
+  train_writer = tf.summary.FileWriter(FLAGS.log_dir + '/train', sess.graph)
+  test_writer = tf.summary.FileWriter(FLAGS.log_dir + '/test')
+  tf.global_variables_initializer().run()
+
+  # Train
+  def feed_dict(train):
+    """Make a TensorFlow feed_dict: maps data onto Tensor placeholders."""
+    if train or FLAGS.fake_data:
+        xs, ys = mnist.train.next_batch(100, fake_data=FLAGS.fake_data)
+        k = FLAGS.dropout
+    else:
+        xs, ys = mnist.test.images, mnist.test.labels
+        k = 1.0
+    return {x: xs, y_: ys, keep_prob: k}
+
+  for i in range(FLAGS.max_steps):
+    if i % 10 == 0:  # Record summaries and test-set accuracy
+      summary, acc = sess.run([merged, accuracy],
+                        feed_dict=feed_dict(False))
+      test_writer.add_summary(summary, i)
+      print('Accuracy at step %s: %s' % (i, acc))
+    else:  # Record train set summaries, and train
+      if i % 100 == 99:  # Record execution stats
+        run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+        run_metadata = tf.RunMetadata()
+        summary, _ = sess.run([merged, train_step],
+                              feed_dict=feed_dict(True),
+                              options=run_options,
+                              run_metadata=run_metadata)
+        train_writer.add_run_metadata(run_metadata, 'step%03d' % i)
+        train_writer.add_summary(summary, i)
+        print('Adding run metadata for', i)
+      else:  # Record a summary
+        summary, _ = sess.run([merged, train_step], feed_dict=feed_dict(True))
+        train_writer.add_summary(summary, i)
+  train_writer.close()
+  test_writer.close()
+
+def main(_):
+  if tf.gfile.Exists(FLAGS.log_dir):
+    tf.gfile.DeleteRecursively(FLAGS.log_dir)
+  tf.gfile.MakeDirs(FLAGS.log_dir)
+  train()
+
+if __name__ == '__main__':
+  parser = initArgParser()
+  FLAGS, unparsed = parser.parse_known_args()
+  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
